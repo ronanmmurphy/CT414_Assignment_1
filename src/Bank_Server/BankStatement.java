a4b226cdb6f9d1e05e50651f5f093f63c3dffdf4
ronanmmurphy <44268431+ronanmmurphy@users.noreply.github.com>
2019-02-08 16:06:51 +0000

    Update BankStatement.java
    update for bank statement

--- src/Bank_Server/BankStatement.java
+++ src/Bank_Server/BankStatement.java
@@ -13,30 +13,41 @@ import java.util.List;
  * @author MacdaraDay
  */
 public class BankStatement implements Statement{
-
+    public int accnum;
+    public Date startDate;
+    public Date endDate;
+    public String accName;
+    public List<Transaction> transactions;
+    public BankStatement(List<Transaction> tran, Date start, Date end,int accnum){
+        this.accnum = accnum;
+        this.startDate = start;
+        this.endDate = end;
+        this.transactions = tran;
+        
+    }
+    
     @Override
     public int getAccountnum() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return accnum;
     }
 
     @Override
     public Date getStartDate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return startDate;
     }
 
     @Override
     public Date getEndDate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return endDate;
     }
 
     @Override
     public String getAccoutName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return accName;
     }
 
     @Override
-    public List getTransations() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Transaction> getTransations() {
+        return transactions;
     }
-    
 }
